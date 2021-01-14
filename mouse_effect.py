2ac4873c3eb8583daf3ce955decb6408983218f9
Nicky <54501296+Nick135ops@users.noreply.github.com>
2021-01-14 21:35:19 +0530

    Update mouse_effect.py

--- mouse_effect.py
+++ mouse_effect.py
@@ -1,5 +1,10 @@
-import mouse
-import playsound
+try:
+    import mouse
+    import playsound
+    import os
+finally:
+    os.system('python -3 -m pip install playsound')
+    os.system('python -3 -m pip install mouse')
 
 while True:
     if mouse.is_pressed():
