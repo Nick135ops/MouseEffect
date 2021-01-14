d8c36067e4c379a3b284622c067783d4c51877f3
Nicky <54501296+Nick135ops@users.noreply.github.com>
2021-01-14 21:36:32 +0530

    Update mouse_sound.py

--- mouse_sound.py
+++ mouse_sound.py
@@ -1,6 +1,9 @@
-from pynput.mouse import Listener
-import playsound
-
+try:
+    from pynput.mouse import Listener
+    import playsound
+finally:
+    os.system('python -3 -m pip install pynput')
+    os.system('python -3 -m pip install playsound')
 def on_click(x, y, button, pressed):
     if pressed:
         playsound.playsound('beep.wav')
